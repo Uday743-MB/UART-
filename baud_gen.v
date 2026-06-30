7a9eacb44031ccda72093e0479dae85916fb74dd
Uday <udaycharan347@gmail.com>
2026-06-30 12:36:53 +0530

    Add baud rate generator module

---
+++ baud_gen.v
@@ -0,0 +1,24 @@
+module baud_gen(
+input clk,
+input rst,
+output reg baud_tick);
+parameter DIV = 434;
+reg [15:0] count;
+always @(posedge clk or posedge rst)
+begin
+    if(rst) begin
+        count <= 0;
+        baud_tick <= 0;
+    end
+    else begin
+        if(count == DIV-1) begin
+            count <= 0;
+            baud_tick <= 1;
+        end
+        else begin
+            count <= count + 1;
+            baud_tick <= 0;
+        end
+    end
+end
+endmodule
